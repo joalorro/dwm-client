5e906a2dc72ee9a807543a7444ee00be8392271c
josue <joalorro@gmail.com>
2023-03-10 16:39:45 -0500

    reorg

---
+++ src/routes/root/root.tsx
@@ -0,0 +1,12 @@
+import './Root.css';
+import { Canvas } from '../../components/canvas';
+
+function Root() {
+  return (
+    <div className="app-background">
+      <Canvas />
+    </div>
+  );
+}
+
+export default Root;
