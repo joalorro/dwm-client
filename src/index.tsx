5ac213003a2932091a570d4cd3ea16ef1ea452c8
josue <joalorro@gmail.com>
2023-03-20 12:32:59 -0400

    removed strict mode for development

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <div className="app-background">
-      <RouterProvider router={router} />
-    </div>
-  </React.StrictMode>,
+  <div className="app-background">
+    <RouterProvider router={router} />
+  </div>,
 );
 
 // If you want to start measuring performance in your app, pass a function
